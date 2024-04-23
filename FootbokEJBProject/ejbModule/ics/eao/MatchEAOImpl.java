6ddf4f84cf348eeb1682c9b1ac55cc0f09e6edfb
Oskar Melbye <oskaraleksanderbm@gmail.com>
2024-04-23 10:46:57 +0200

    Updated classes

--- FootbokEJBProject/ejbModule/ics/eao/MatchEAOImpl.java
+++ FootbokEJBProject/ejbModule/ics/eao/MatchEAOImpl.java
@@ -1,25 +1,56 @@
 package ics.eao;
 
+import java.util.List;
+
 import ics.ejb.Match;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 /**
  * Session Bean implementation class Match
  */
 @Stateless
 public class MatchEAOImpl implements MatchEAOLocal {
-
-	private Match match;
+	@PersistenceContext(unitName = "FootBookEJBSql")
+	private EntityManager em;
 	
     public MatchEAOImpl() {
         // TODO Auto-generated constructor stub
     }
-
-	public Match getMatch() {
-		return match;
+    
+    public Match findByMatchId(long id) {
+		return em.find(Match.class, id);
 	}
 
-	public void setMatch(Match match) {
-		this.match = match;
+	public Match createMatch(Match m) {
+		em.persist(m);
+		return m;
+	}
+
+	public Match updateMatch(Match m) {
+		em.merge(m);
+		return m;
+	}
+
+	public void deleteMatch(long id) {
+		Match m = this.findByMatchId(id);
+		if (m != null) {
+			em.remove(m);
+		}
+	}
+
+	public List<Match> findAll() {
+		TypedQuery<Match> query = em.createNamedQuery("Match.findAll", Match.class);
+		List<Match> results = query.getResultList();
+		return results;
+	}
+
+	public List<Match> findByPitchId(String pitchId) {
+		TypedQuery<Match> query = em.createNamedQuery("Match.findByPitchId", Match.class);
+		query.setParameter("pitchId", pitchId);
+		List<Match> results = query.getResultList();
+		return results;
 	}
 }
