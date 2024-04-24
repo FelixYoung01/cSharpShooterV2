8923a147a3d342a6a72dc3665c4fb4f541e08ab6
FelixYoung01 <118141366+FelixYoung01@users.noreply.github.com>
2024-04-24 10:11:44 +0200

    Added the basic NamedQueries
    For classes Referee and team

--- FootbokEJBProject/ejbModule/ics/ejb/Referee.java
+++ FootbokEJBProject/ejbModule/ics/ejb/Referee.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 
 @Entity
-@NamedQuery(name="Referee.findAll", query= "SELECT r FROM Referee r")
+@NamedQueries({
+	@NamedQuery(name="Referee.findAll", query= "SELECT r FROM Referee r")})
+
 @Table(name="Referee")
 public class Referee implements Serializable {
 	private static final long serialVersionUID = 1L;
