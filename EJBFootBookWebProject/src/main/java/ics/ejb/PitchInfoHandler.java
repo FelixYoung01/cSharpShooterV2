5b4c86c39d387d82b54d623c3afaaf6108c73abb
JususAJH <alex@osteopatenkh.se>
2024-05-14 13:12:15 +0200

    Fixed the user count on each match.

--- EJBFootBookWebProject/src/main/java/ics/ejb/PitchInfoHandler.java
+++ EJBFootBookWebProject/src/main/java/ics/ejb/PitchInfoHandler.java
@@ -1,6 +1,8 @@
 package ics.ejb;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import facade.FacadeLocal;
@@ -14,22 +16,36 @@ public class PitchInfoHandler implements IPathHandler {
 	@Override
 	public RequestDispatcher handleRequestDispatcherPost(HttpServletRequest request, HttpServletResponse response,
 			FacadeLocal facade) throws ServletException, IOException {
-		
+
 		String pitchId = request.getParameter("pitchId");
 		Pitch pitch = facade.findPitch(pitchId);
-		
+
 		Set<Match> matchesOnPitch = facade.getMatchesOnPitch(pitchId);
+		Map<String, Integer> matchUserCount = new HashMap<>();
+
+		for (Match match : matchesOnPitch) {
+			
+			Set<User> usersOnMatch = facade.getUsersOnMatch(match.getMatchId());
+			
+				int userCount = usersOnMatch.size();
+				matchUserCount.put(match.getMatchId(), userCount);
+			}
 		
+			if (matchUserCount.isEmpty()) {
+				System.out.println("No users on pitch");
+			}
+
+		request.setAttribute("matchUserCount", matchUserCount);
 		request.setAttribute("matchesOnPitch", matchesOnPitch);
 		request.setAttribute("pitch", pitch);
-		
+
 		return request.getRequestDispatcher("/pitchInfo.jsp");
 	}
-	
+
 	@Override
 	public RequestDispatcher handleRequestDispatcherGet(HttpServletRequest request, HttpServletResponse response,
-            FacadeLocal facade) throws ServletException, IOException {
-        return request.getRequestDispatcher("/pitchInfo.jsp");
-    }
-	
+			FacadeLocal facade) throws ServletException, IOException {
+		return request.getRequestDispatcher("/pitchInfo.jsp");
+	}
+
 }
